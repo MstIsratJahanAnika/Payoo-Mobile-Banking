2889e0e0ca135f3ab067588ed135591858489548
MstIsratJahanAnika <mstisratjahananika@gmail.com>
2026-02-19 17:26:33 +0600

    home page

---
+++ script/cashout.js
@@ -0,0 +1,56 @@
+/**
+ * steps:
+ * 1. get agent number and validate
+ * 2. get amount, validate, convert to Number
+ * 3. get currentBalance, validate, convert to Number
+ * 4. calculate new balance
+ * 5. get the pin and verify
+ * 5.1 true::> give alert -> add current to remaining balance
+ * 5.2 false::> give alert -> return
+ */
+
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+
+    // 1. get agent number and validate
+    const agentNumberInput = document.getElementById('agent-number');
+    const agentNumber = agentNumberInput.value;
+    console.log(agentNumber);
+    if(agentNumber.length !== 11){
+        alert('Invalid Agent Number');
+        return;
+    }
+
+    // 2. get amount, validate, convert to Number
+    const cashoutNumberInput = document.getElementById('amount');
+    const cashoutNumber = cashoutNumberInput.value;
+    console.log(cashoutNumber);
+
+    // 3. get currentBalance, validate, convert to Number
+    const balanceElement = document.getElementById('balance');
+    const balance = balanceElement.innerText //input field na pre-assigned value
+    // console.log(balance);  //original balance
+
+    // 4. calculate new balance
+    const newBalance = Number(balance) - Number(cashoutNumber);  //.value, string a thake number a convert korte hobe
+    if(newBalance < 0){
+        alert('Invalid Amount');
+        return;
+    }
+
+    // 5. get the pin and verify
+    cashoutPinInput = document.getElementById('pin-number');
+    const pin = cashoutPinInput.value;
+    if(pin === '1234'){
+        // 5.1 true::> give alert -> add current to remaining balance
+        alert('Cashout successful');
+        console.log("new balance", newBalance);
+        balanceElement.innerText = newBalance;
+    }
+    else{
+        alert('Invalid Pin');
+        return;
+    }
+    
+
+
+});
